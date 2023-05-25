9870052c17efb6effbd053cc324521ac8f783e3d
panzerfaust <panzerfaust@ev0lve.xyz>
2023-05-25 17:23:37 +0500

    Added default value to struct, fixed a bug on earlier php versions

--- src/Admin/Fields/FieldTable.php
+++ src/Admin/Fields/FieldTable.php
@@ -7,6 +7,44 @@ use Simflex\Core\Container;
 
 class FieldTable extends Field
 {
+    public const STRUCT_VALUE = [
+        [
+            'n' => 'n',
+            't' => 'text',
+            'l' => 'Имя',
+            'v' => '',
+            'e' => '',
+        ],
+        [
+            'n' => 't',
+            't' => 'combo',
+            'l' => 'Тип',
+            'v' => 'text',
+            'e' => 'text=Текст,,int=Число,,combo=Список,,editor=Редактор,,image=Изображение,,file=Файл',
+        ],
+        [
+            'n' => 'l',
+            't' => 'text',
+            'l' => 'Заголовок',
+            'v' => '',
+            'e' => '',
+        ],
+        [
+            'n' => 'v',
+            't' => 'text',
+            'l' => 'Значение',
+            'v' => '',
+            'e' => '',
+        ],
+        [
+            'n' => 'e',
+            't' => 'text',
+            'l' => 'Дополнительно',
+            'v' => '',
+            'e' => '',
+        ],
+    ];
+
     public function input($value)
     {
         // fallback value = []
@@ -21,7 +59,7 @@ class FieldTable extends Field
         }
 
         $tmpVal = json_decode($value, true);
-        if (!str_contains(Container::getRequest()->getPath(), 'content_template_param') &&
+        if (!strstr(Container::getRequest()->getPath(), 'content_template_param') &&
             $this->params['struct'] && $tmpVal['s'] != $this->params['struct']) {
             $s = json_decode($this->params['struct'], true);
             $v = json_decode($value, true);
