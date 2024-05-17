3fcb371cd3c971da47432898daa158065f6c683f
pf <panzerfaust@ev0lve.xyz>
2024-05-17 15:21:05 +0500

    Removed favicon, bumped version

--- src/constants.php
+++ src/constants.php
@@ -7,7 +7,7 @@ const SF_LOCATION_API = 'api';
 
 const SF_CORE_ROOT_PATH = __DIR__;
 
-const SF_VERSION = '3.1.7';             // core version.feature version.patch version
-const SF_VERSION_DATE = '12.07.2023';   // publish date
+const SF_VERSION = '3.2.0';             // core version.feature version.patch version
+const SF_VERSION_DATE = '17.05.2024';   // publish date
 
 const IMAGETYPE_SVG = 'svg';
